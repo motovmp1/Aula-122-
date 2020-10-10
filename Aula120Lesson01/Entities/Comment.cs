594fe8199fd9f400b87aa6365e4fb1515c5dacbf
motovmp1 <vinicius.mpinho@gmail.com>
2020-10-10 17:37:53 +0100

    Aula 122 string builder

---
+++ Aula120Lesson01/Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula120Lesson01
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ Aula120Lesson01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Aula120Lesson01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("That is awesome!");
+
+            Post p1 = new Post
+                (
+                DateTime.Parse("21/06/2018 13:45:44"),
+                "Travel to New Zealand",
+                "I am going to visit country",
+                12
+                );
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Console.WriteLine(p1);
+
+
+
+
+
+        }
+    }
+}
